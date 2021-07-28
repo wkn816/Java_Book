62a52ff6ac18e3733a9acebb86bf63fd78909bb4
wkn816 <wkn816@gmail.com>
2021-07-29 00:52:11 +0900

    List2-９演算子

---
+++ List2_9.java
@@ -0,0 +1,14 @@
+class List2_9 {
+	public static void main (String[] args) {
+
+  int i = 11;
+  i++ ;
+  i /= 2 ;
+  System.out.println("iの値は" + i + "です");
+    //i=6
+  int j ;
+  j = i + i ;
+    System.out.println("iの値は" + j + "です");
+    //j=36
+	}
+}
