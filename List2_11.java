a8f0252e29aea7633b75ba09c382353d39cb016b
wkn816 <wkn816@gmail.com>
2021-07-29 01:02:36 +0900

    List2_11キャスト

---
+++ List2_11.java
@@ -0,0 +1,9 @@
+class List2_11 {
+	public static void main (String[] args) {
+
+    double d = 1.5 ;
+    int i = (int)d ;
+    System.out.println("dの値は" + d);
+    System.out.println("iの値は" + i);
+	}
+}
