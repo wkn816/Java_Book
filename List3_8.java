47cf663c36c79b8f5c15fa3139a70125cd0aaf25
wkn816 <wkn816@gmail.com>
2021-07-30 21:31:04 +0900

    List3-8whileループ

--- List3_8.java
+++ List3_8.java
@@ -1,10 +1,10 @@
 class List3_8 {
 	public static void main (String[] args) {
 
-    int i = 0;
-    while( i < 5 ){
-      System.out.println( i + "回目のWhile");
-      i++;
+    int i = 5;
+    while( i > 0 ){
+      System.out.println( i );
+      i--;
     }
 	}
 }
