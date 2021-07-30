d852aa42f1d8cbe13f66a31c8bbe3cbafa7ed2e3
wkn816 <wkn816@gmail.com>
2021-07-30 21:39:12 +0900

    List3-9do~while文

---
+++ List3_9.java
@@ -0,0 +1,12 @@
+    class List3_9 {
+	public static void main (String[] args) {
+
+    int b = 5;
+    do{
+        System.out.println( "do_whileの文" + b);
+        b--;
+    }while( b > 0);
+
+	}
+}
+//do~whileの場合true/falseに関わらず一回は処理が行われる。
